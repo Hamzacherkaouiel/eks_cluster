8101ed54d650b0e95e0f41032f4f4a1042ffd4d1
hamza <hamza.elmalki1234@gmail.com>
2025-09-12 22:25:34 +0100

    create eks

---
+++ terraform-vps/eks.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "eks" {
+  name = "${var.env}-${var.eks_name}-eks-cluster"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement":[
+     {
+       "Effect": "Allow",
+       "Action": "sts:AssumeRole",
+       "Principal":{
+          "Service": "eks.amazonaws.com"
+       }
+     }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "${var.env}-${var.eks_name}"
+  role_arn = aws_iam_role.eks.arn
+  version = var.eks_version
+  vpc_config {
+    subnet_ids = [aws_subnet.private_zone1.id,aws_subnet.private_zone2.id]
+    endpoint_private_access = false
+    endpoint_public_access = true
+  }
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+  depends_on = [aws_iam_role_policy_attachment.eks]
+}
